4b327c960a0463e0f7c1afe1b55989996101352e
romanrusset <juanromaan14@gmail.com>
2025-02-17 16:51:58 +0100

    Fixing some test evaluation problems

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,2 @@
-using TFG
-using Test
-
-
-@testset "TFG.jl" begin
-    @test my_f(2,1) == 7
-    @test my_f(2,3) == 13
-end
-
-@testset "Derivative Tests" begin
-    @test derivative_of_my_f(2,1) == 2
-end
+using SafeTestsets
+@safetestset "My f test" begin include("my_f_tests.jl") end
