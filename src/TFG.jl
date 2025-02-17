58077bf030a61739a4656484a0340b8d36c23052
romanrusset <juanromaan14@gmail.com>
2025-02-17 15:34:54 +0100

    add ForwardDiff dependency and setup safetestsets

--- src/TFG.jl
+++ src/TFG.jl
@@ -1,7 +1,9 @@
 module TFG
 
+using ForwardDiff
+
 include("file_example.jl")
 
-export my_f
+export my_f, derivative_of_my_f
 
 end
--- test/my_f_tests.jl
+++ test/my_f_tests.jl
@@ -1,7 +1,6 @@
 using TFG
 using Test
 
-
 @testset "TFG.jl" begin
     @test my_f(2,1) == 7
     @test my_f(2,3) == 13
